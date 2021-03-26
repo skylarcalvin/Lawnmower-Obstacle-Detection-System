4aadae1d7b6cd7cd6b87a2cfec3a2af97cc7f202
skylarcalvin <rducky01@gmail.com>
2021-03-26 11:12:12 -0500

    Added image capture script

---
+++ image_capture.py
@@ -0,0 +1,25 @@
+import cv2
+
+# Create video capture object.
+video = cv2.VideoCapture(0)
+
+# Input for while loop.
+result = True
+
+# Iterator for the image name.
+n = 1
+
+while(result):
+
+    # Form image name.
+    imageName = "image" + str(n) + ".jpg"
+    
+    # Capture videm frame and write it to the file.
+    ret, frame = video.read()
+    cv2.imwrite(imageName)
+
+result = input('Please enter "False" here.')
+
+# Turn off video capture.
+video.release()
+cv2.destroyAllWindows()
